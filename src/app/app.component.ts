61370b72818df34b124317f00062b12adff5bbc1
Rogerio Castellano <rc_publico-github@yahoo.com.br>
2025-02-22 17:16:52 +0100

    Add ExpensesFilter with dummy categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ExpenseListComponent } from './expense-list/expense-list.component';
+import { ExpenseFilterComponent } from './expense-filter/expense-filter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ExpenseListComponent, ExpenseFilterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-expense-tracker';
---
+++ src/app/expense-filter/expense-filter.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-filter',
+  imports: [],
+  templateUrl: './expense-filter.component.html',
+  styleUrl: './expense-filter.component.css'
+})
+export class ExpenseFilterComponent {
+
+}
