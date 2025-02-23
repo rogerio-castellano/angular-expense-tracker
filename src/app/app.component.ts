a229def211454901d2da47274e5abdacaa5ba015
Rogerio Castellano <rc_publico-github@yahoo.com.br>
2025-02-23 16:31:00 +0100

    Add Header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ExpenseListComponent } from './expense-list/expense-list.component';
 import { ExpenseFilterComponent } from './expense-filter/expense-filter.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ExpenseListComponent, ExpenseFilterComponent],
+  imports: [
+    RouterOutlet,
+    ExpenseListComponent,
+    ExpenseFilterComponent,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
